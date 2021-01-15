5ec82797379b83ff208f1d70235583b9b22635ae
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-01-14 23:52:58 -0500

    adding ForceNetworkLocation

--- common/params_pyx.pyx
+++ common/params_pyx.pyx
@@ -70,6 +70,8 @@ keys = {
   b"Offroad_NeosUpdate": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_UpdateFailed": [TxType.CLEAR_ON_MANAGER_START],
   b"Offroad_HardwareUnsupported": [TxType.CLEAR_ON_MANAGER_START],
+  
+  b"ForceNetworkLocation": [TxType.PERSISTENT],
 }
 
 def ensure_bytes(v):
