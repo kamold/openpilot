9e21e1fbf304e5fbdcc7911e0fde22a2fe227c28
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-01-18 17:37:30 -0500

    adding some more code from 0.7.9

--- common/basedir.py
+++ common/basedir.py
@@ -4,5 +4,7 @@ BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__
 from selfdrive.hardware import PC
 if PC:
   PERSIST = os.path.join(BASEDIR, "persist")
+  PARAMS = os.path.join(BASEDIR, "persist", "params")
 else:
   PERSIST = "/persist"
+  PARAMS = "/data/params"
